dfaf728de3e2ca26f480ab0c163177dae90d9728
Vinicius <vinicius.katsuaki13@hotmail.com>
2023-04-18 15:37:46 -0300

    api-loading-type

--- src/pages/Main/Drinks/index.tsx
+++ src/pages/Main/Drinks/index.tsx
@@ -1,11 +1,29 @@
+import { useEffect, useState } from 'react'
+
 import { Head } from '../../../components/Head'
+import { Snacks } from '../../../components/Snacks'
 import { SnackTitle } from '../../../components/SnackTitle'
 
+import { SnackData } from '../../../interfaces/SnackData'
+
+import { getDrinks } from '../../../services/api'
+
 export default function Drinks() {
+  const [drinks, setDrinks] = useState<SnackData[]>([])
+
+  useEffect(() => {
+    (async () => {
+      const dirnksRequest = await getDrinks()
+
+      setDrinks(dirnksRequest.data)
+    })()
+  }, [])
+
   return(
     <>
       <Head title='Bebidas' description='Nossos melhores bebidas' />
       <SnackTitle>Bebidas</SnackTitle>
+      <Snacks snacks={drinks}></Snacks>
     </>
   )
 }
--- src/pages/Main/Pizzas/index.tsx
+++ src/pages/Main/Pizzas/index.tsx
@@ -1,11 +1,29 @@
+import { useEffect, useState } from 'react'
+
 import { Head } from '../../../components/Head'
+import { Snacks } from '../../../components/Snacks'
 import { SnackTitle } from '../../../components/SnackTitle'
 
+import { SnackData } from '../../../interfaces/SnackData'
+
+import { getPizzas } from '../../../services/api'
+
 export default function Pizzas() {
+  const [pizzas, setPizzas] = useState<SnackData[]>([])
+
+  useEffect(() => {
+    (async () => {
+      const pizzaRequest = await getPizzas()
+
+      setPizzas(pizzaRequest.data)
+    })()
+  }, [])
+
   return(
     <>
       <Head title='Pizzas' description='Nossos melhorers pizzas' />
       <SnackTitle>Pizzas</SnackTitle>
+      <Snacks snacks={pizzas}></Snacks>
     </>
   )
 }
